6eb3b1d7fb9089501887d050ae5fa47b1d832021
WORK-PC <soboiev86rus@mail.ru>
2022-12-11 21:57:31 +0400

    Added series calculation

--- main.py
+++ main.py
@@ -1,24 +1,73 @@
 import os
 
-import cv2
-import numpy
+import numpy as np
 
 from utils.consts import M, SIGMA
-from utils.in_out import read_image, write_image
-from utils.nzb import svi_1_encode, svi_1_encode_changed
+from utils.in_out import read_image
+from utils.nzb import svi_1_encode, get_plane
 from utils.watermark import generate_watermark
 
+
+def get_series_array(bit_plate, max_i):
+
+    bit_plate_flatten = bit_plate.copy().flatten()
+
+    result = np.zeros(max_i)
+
+    current_bit = 0
+    series_length = 0
+    while current_bit < bit_plate_flatten.size:
+        for series_bit in range (current_bit, bit_plate_flatten.size):
+            if bit_plate_flatten[current_bit] == bit_plate_flatten[series_bit]:
+                series_length += 1
+            else:
+                result[series_length - 1] += 1
+                current_bit += series_length
+                series_length = 0
+                break
+            if series_bit == bit_plate_flatten.size - 1:
+                current_bit = bit_plate_flatten.size
+
+    return result
+
+
 if __name__ == '__main__':
-    images = os.listdir('resources/images')
 
-    for image_file_name in images:
+    images_file_names = os.listdir('resources/images')
+    bit_plate_num = 2
 
+    K = np.array(images_file_names).size
+
+    features_vector = []
+    processed_images = []
+
+    image_num = 0
+    for image_file_name in images_file_names:
         full_image_path = 'resources/images/' + image_file_name
         image = read_image(full_image_path)
-        watermark = generate_watermark(image.size, M, SIGMA).reshape(image.shape[0], image.shape[1])
 
-        result = svi_1_encode_changed(image, watermark, 7)
-        write_image(result, 'resources/result/' + image_file_name)
+        if image_num % 10 == 0:
+            print(f'Processing {image_num}%')
+
+        image_to_process = ''
+        if image_num < K / 2:
+            watermark = generate_watermark(image.size, M, SIGMA).reshape(image.shape[0], image.shape[1])
+            image_to_process = svi_1_encode(image, watermark, bit_plate_num)
+            #write_image(image_with_watermark, 'resources/result/' + image_file_name)
+        else:
+            image_to_process = image
+
+        bit_plate = get_plane(image_to_process, bit_plate_num)
+        unwrapped_bit_plate = bit_plate.flatten()
+        #relative_frequencies = get_relative_freq_3(unwrapped_bit_plate)
+        series_array = get_series_array(bit_plate, 100)
         a = 3
+        #features_vector.append(relative_frequencies)
+        #processed_images.append(image_to_process)
+
+        image_num += 1
+
+    a = 3
+
 
 
--- utils/nzb.py
+++ utils/nzb.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def get_plane(channel_image, plane_num):
-    return channel_image & (2 ** (plane_num - 1))
+def get_plane(image, plane_num):
+    return ((image & (2 ** (plane_num - 1))) / (2 ** (plane_num - 1))).astype('uint8')
 
 
-def svi_1_encode_changed(original_image, watermark, bit_plate_num):
+def svi_1_encode(original_image, watermark, bit_plate_num):
     num_for_clear_bit_plate = 255 - (2 ** (bit_plate_num - 1))
     prepared_watermark_colored = ((watermark / 255) * (2 ** (bit_plate_num - 1))).astype(np.uint8)
     channel_with_empty_bit_plate = original_image & num_for_clear_bit_plate
