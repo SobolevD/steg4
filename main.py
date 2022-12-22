1f1467684d3ece5c920c435c5ddb5e0ae440ac54
WORK-PC <soboiev86rus@mail.ru>
2022-12-23 00:53:38 +0400

    Lab4 ready

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
+import sklearn
+import sklearn.discriminant_analysis
+import sklearn.ensemble
+import sklearn.svm
+import sklearn.tree
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
 
 from utils.consts import M, SIGMA
 from utils.in_out import read_image
 from utils.nzb import svi_1_encode, get_plane
 from utils.watermark import generate_watermark
+
+
+def train_and_test(X, y, clf):
+    # Отделяем 20% выборки для обучения и 80% для оценки
+    X_train, X_test, y_train, y_test = \
+        sklearn.model_selection.train_test_split(X, y, train_size=0.2, random_state=42)
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    return sklearn.metrics.f1_score(y_pred, y_test)
 
 
 def get_series_array(bit_plate, max_i):
@@ -18,7 +36,7 @@ def get_series_array(bit_plate, max_i):
     series_length = 0
     while current_bit < bit_plate_flatten.size:
         for series_bit in range (current_bit, bit_plate_flatten.size):
-            if bit_plate_flatten[current_bit] == bit_plate_flatten[series_bit]:
+            if bit_plate_flatten[current_bit] == bit_plate_flatten[series_bit] and series_length < max_i:
                 series_length += 1
             else:
                 result[series_length - 1] += 1
@@ -31,43 +49,86 @@ def get_series_array(bit_plate, max_i):
     return result
 
 
+def get_feature_vector(series):
+    tmp = np.array(np.split(series, 5)) # Чтоб было по 4 штуки
+    result = np.zeros(5)
+    for i in range(tmp.shape[0]):
+        result[i] = np.sum(np.array(tmp[i]))
+    return result
+
+
 if __name__ == '__main__':
-
     images_file_names = os.listdir('resources/images')
-    bit_plate_num = 2
 
+    dt = []
+    rf = []
+    mlp = []
+    ab = []
+
+    p = 2
     K = np.array(images_file_names).size
+    y = np.zeros(K)
 
-    features_vector = []
-    processed_images = []
+    q_array = [1, 0.7, 0.5, 0.3, 0.1]
+    for q in q_array:
 
-    image_num = 0
-    for image_file_name in images_file_names:
-        full_image_path = 'resources/images/' + image_file_name
-        image = read_image(full_image_path)
+        train_dataset = []
+        print(f'q={q}')
 
-        if image_num % 10 == 0:
-            print(f'Processing {image_num}%')
+        image_num = 0
 
-        image_to_process = ''
-        if image_num < K / 2:
-            watermark = generate_watermark(image.size, M, SIGMA).reshape(image.shape[0], image.shape[1])
-            image_to_process = svi_1_encode(image, watermark, bit_plate_num)
-            #write_image(image_with_watermark, 'resources/result/' + image_file_name)
-        else:
-            image_to_process = image
+        for image_file_name in images_file_names:
 
-        bit_plate = get_plane(image_to_process, bit_plate_num)
-        unwrapped_bit_plate = bit_plate.flatten()
-        #relative_frequencies = get_relative_freq_3(unwrapped_bit_plate)
-        series_array = get_series_array(bit_plate, 100)
-        a = 3
-        #features_vector.append(relative_frequencies)
-        #processed_images.append(image_to_process)
+            if image_num % 10 == 0:
+                print(f'Processing {image_num*2}%')
 
-        image_num += 1
+            full_image_path = 'resources/images/' + image_file_name
+            image = read_image(full_image_path)
 
-    a = 3
+            image_to_process = ''
+            if image_num < K / 2:
+                watermark = generate_watermark(image.size, M, SIGMA).reshape(image.shape[0], image.shape[1])
+                image_to_process = svi_1_encode(image, watermark, p, q)
+            else:
+                image_to_process = image
+                y[image_num] = 1
+
+            bit_plate = get_plane(image_to_process, p)
+            series_array = get_series_array(bit_plate, 20)  # / image.size
+            feature_vector = get_feature_vector(series_array)
+            train_dataset.append(feature_vector)
+
+            image_num += 1
+
+        decision_tree_result = train_and_test(train_dataset, y, sklearn.tree.DecisionTreeClassifier(max_depth=5))
+        random_forest_result = train_and_test(train_dataset, y, sklearn.ensemble.RandomForestClassifier(n_estimators=100, max_depth=1000))
+        mlp_result = train_and_test(train_dataset, y, MLPClassifier(alpha=1, max_iter=1000))
+        ada_boost_result = train_and_test(train_dataset, y, AdaBoostClassifier(n_estimators=50))
+        print("====================================================")
+        print(f'Decision tree classifier accuracy: {decision_tree_result}')
+        print(f'Random forest classifier accuracy: {random_forest_result}')
+        print(f'MLP classifier accuracy: {mlp_result}')
+        print(f'AdaBoost classifier accuracy: {ada_boost_result}')
+        print("====================================================")
+        dt.append(decision_tree_result)
+        rf.append(random_forest_result)
+        mlp.append(mlp_result)
+        ab.append(ada_boost_result)
+
+    plt.plot(dt, q_array)
+    plt.plot(rf, q_array)
+    plt.plot(mlp, q_array)
+    plt.plot(ab, q_array)
+    plt.show()
+
+    plt.plot(q_array, dt, label='Decision Tree Classifier')
+    plt.plot(q_array, rf, label='Random Forest Classifier')
+    plt.plot(q_array, mlp, label='MLP Classifier')
+    plt.plot(q_array, ab, label='AdaBoost Classifier')
+    plt.xlabel('q')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
 
 
 
--- utils/nzb.py
+++ utils/nzb.py
@@ -5,9 +5,17 @@ def get_plane(image, plane_num):
     return ((image & (2 ** (plane_num - 1))) / (2 ** (plane_num - 1))).astype('uint8')
 
 
-def svi_1_encode(original_image, watermark, bit_plate_num):
+def svi_1_encode(original_image, watermark, bit_plate_num, q):
     num_for_clear_bit_plate = 255 - (2 ** (bit_plate_num - 1))
-    prepared_watermark_colored = ((watermark / 255) * (2 ** (bit_plate_num - 1))).astype(np.uint8)
-    channel_with_empty_bit_plate = original_image & num_for_clear_bit_plate
+    prepared_watermark = np.random.randint(0, 2, size=watermark.size).astype(np.uint8)
+    image_with_empty_bit_plate = original_image & num_for_clear_bit_plate
 
-    return channel_with_empty_bit_plate | prepared_watermark_colored
+    result = np.zeros(original_image.size)
+
+    right_border = int(q * original_image.size)
+    result[0:right_border] = prepared_watermark.flatten()[0:right_border]
+
+    new_plane = get_plane(original_image, bit_plate_num)
+    result[right_border:] = new_plane.flatten()[right_border:]
+
+    return image_with_empty_bit_plate | ((result.astype('uint8')).reshape(original_image.shape[0], original_image.shape[1]) * (2 ** (bit_plate_num - 1)))
